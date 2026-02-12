19e7b30f33ac42d11869412fee423e6832969fb2
Isuru Fernando <isuruf@gmail.com>
2026-02-11 18:58:01 -0800

    remove hack

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ex
 
-if [[ -f "$BUILD_PREFIX/meson_cross_file.txt" ]]; then
-    # Extend meson_cross_file to use host python;
-    # requires that [binaries] section is last in meson_cross_file
-    echo "python = '${PREFIX}/bin/python'" >> $BUILD_PREFIX/meson_cross_file.txt
-fi
-
 $PYTHON -m pip install . -vv --no-build-isolation --no-deps \
     -Cbuilddir=builddir \
     -Csetup-args=${MESON_ARGS// / -Csetup-args=} \
